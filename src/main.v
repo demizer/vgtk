bad7ef0af3bdee037d5cd893d3d5243f64e05da4
Jesus Alvarez <jeezusjr@gmail.com>
2023-09-23 23:04:28 -0700

    Refactory namespace loading a bit

--- src/main.v
+++ src/main.v
@@ -2,12 +2,29 @@ module main
 
 import walkingdevel.vxml
 import os
-import regex
 
 struct GtkIRNamespace {
-	name     string
-	path     string
-	includes []string
+	name         string
+	package_name string
+	path         string
+	includes     []string
+	version      string
+	parsed       vxml.Node
+}
+
+fn (g GtkIRNamespace) filter_by_class_name(name string) ?[]&vxml.Node {
+	classes := g.parsed.get_elements_by_predicate(fn (node &vxml.Node) bool {
+		return node.name == 'class'
+	})
+	for cls in classes {
+		if cls.attributes['name'] == name {
+			return [cls]
+		}
+	}
+	if name == '' {
+		return classes
+	}
+	return none
 }
 
 type Imports = []GtkIRNamespace
@@ -22,6 +39,7 @@ fn (i Imports) has_import(path string) bool {
 }
 
 struct Parser {
+	root    GtkIRNamespace
 	imports Imports
 }
 
@@ -38,45 +56,16 @@ fn (p Parser) unique_imports() []string {
 	return imps
 }
 
-fn package_name_from_file(content string) !string {
-	query := r'package name="(?P<name>\w+)"'
-	mut re := regex.new()
-	re.compile_opt(query)!
-	mut name := ''
-	for elem in re.find_all_str(content) {
-		re.match_string(elem)
-		name = re.get_group_by_name(elem, 'name')
-	}
-	return name
-}
-
-fn includes_from_file(content string) ![]string {
-	query := r'include name="(?P<name>\w+)" version="(?P<version>[\w\.]+)"'
-	mut re := regex.new()
-	re.compile_opt(query)!
-	mut includes := []string{}
-	for elem in re.find_all_str(content) {
-		re.match_string(elem)
-		name := re.get_group_by_name(elem, 'name')
-		version := re.get_group_by_name(elem, 'version')
-		includes << '/usr/share/gir-1.0/${name}-${version}.gir'
-	}
-	return includes
-}
-
 fn recurse_imports(mut imports Imports, start GtkIRNamespace) !Imports {
 	imports << start
 	for v in start.includes {
 		// TODO: only read first 10kb of the file to make it faster
-		content := os.read_file(v)!
-		includes := includes_from_file(content)!
-		pname := package_name_from_file(content)!
-		tmp := GtkIRNamespace{
-			name: pname
-			path: v
-			includes: includes
+		if !os.exists(v) {
+			eprintln('import: ${v} does not exist! Skipping')
+			continue
 		}
-		for i in includes {
+		tmp := new_gir_namespace(v)!
+		for i in tmp.includes {
 			if imports.has_import(i) {
 				continue
 			}
@@ -87,64 +76,76 @@ fn recurse_imports(mut imports Imports, start GtkIRNamespace) !Imports {
 }
 
 fn new_parser() !&Parser {
-	gir := new_gtk_ir_namespace('/usr/share/gir-1.0/Gtk-4.0.gir')!
-	mut imps := Imports([]GtkIRNamespace{})
-	imps = recurse_imports(mut imps, gir)!
+	gir := new_gir_namespace('/usr/share/gir-1.0/Gtk-4.0.gir')!
+	// println(gir)
+	// mut imps := Imports([]GtkIRNamespace{})
+	// imps = recurse_imports(mut imps, gir)!
 	return &Parser{
-		imports: imps
+		root: gir
+		// imports: imps
 	}
 }
 
-fn new_gtk_ir_namespace(path string) !&GtkIRNamespace {
-	content := os.read_file(path)!
-	includes := includes_from_file(content)!
-	pname := package_name_from_file(content)!
+fn new_gir_namespace(path string) !&GtkIRNamespace {
+	parsed := vxml.parse_file(path)!
+	pincludes := parsed.get_elements_by_predicate(fn (node &vxml.Node) bool {
+		return node.name == 'include'
+	})
+	mut includes := []string{}
+	for i in pincludes {
+		name := i.attributes['name']
+		version := i.attributes['version']
+		includes << '/usr/share/gir-1.0/${name}-${version}.gir'
+	}
+	pname := parsed.get_elements_by_predicate(fn (node &vxml.Node) bool {
+		return node.name == 'namespace'
+	})
+	ppackage := parsed.get_elements_by_predicate(fn (node &vxml.Node) bool {
+		return node.name == 'package'
+	})
 	return &GtkIRNamespace{
-		name: pname
+		name: pname.first().attributes['name']
+		version: pname.first().attributes['version']
+		package_name: ppackage.first().attributes['name']
 		path: path
+		parsed: parsed
 		includes: includes
 	}
 }
 
 fn main() {
-	// gir_p := new_parser() or { panic(err) }
-	// println(gir_p.unique_imports())
-	//
-	ggir := vxml.parse_file('/usr/share/gir-1.0/Gtk-4.0.gir') or { panic(err) }
+	gir_p := new_parser() or { panic(err) }
+	println(gir_p.root.filter_by_class_name('Label'))
 
-	classes := ggir.get_elements_by_predicate(fn (node &vxml.Node) bool {
-		return node.name == 'class'
-	})
-
-	// println(posts.first().get_text())
-	for cls in classes {
-		// println('mac: ${mac}')
-		name := cls.attributes['name']
-		typ := cls.attributes['c:type']
-		// println('name: ${name}')
-		// println('type: ${typ}')
-		if name == 'Application' {
-			// println(cls.children)
-			cons := cls.get_elements_by_predicate(fn (node &vxml.Node) bool {
-				return node.name == 'constructor'
-			})
-			ret_type := '&C.${typ}'
-			mut param_str := ''
-			params := cons[0].children.last().children
-			for i, param in params {
-				param_str += param.attributes['name'] + ' '
-				typ2 := param.children.last()
-				param_str += match typ2.attributes['c:type'] {
-					'const char*' { '&char' }
-					else { typ2.attributes['c:type'] }
-				}
-				if i < params.len - 1 {
-					param_str += ', '
-				}
-			}
-			print('fn C.${cons[0].attributes['c:identifier']}(${param_str}) ${ret_type}')
-		}
-	}
+	// // println(posts.first().get_text())
+	// for cls in classes {
+	// 	// println('mac: ${mac}')
+	// 	name := cls.attributes['name']
+	// 	typ := cls.attributes['c:type']
+	// 	// println('name: ${name}')
+	// 	// println('type: ${typ}')
+	// 	if name == 'Application' {
+	// 		// println(cls.children)
+	// 		cons := cls.get_elements_by_predicate(fn (node &vxml.Node) bool {
+	// 			return node.name == 'constructor'
+	// 		})
+	// 		ret_type := '&C.${typ}'
+	// 		mut param_str := ''
+	// 		params := cons[0].children.last().children
+	// 		for i, param in params {
+	// 			param_str += param.attributes['name'] + ' '
+	// 			typ2 := param.children.last()
+	// 			param_str += match typ2.attributes['c:type'] {
+	// 				'const char*' { '&char' }
+	// 				else { typ2.attributes['c:type'] }
+	// 			}
+	// 			if i < params.len - 1 {
+	// 				param_str += ', '
+	// 			}
+	// 		}
+	// 		print('fn C.${cons[0].attributes['c:identifier']}(${param_str}) ${ret_type}')
+	// 	}
+	// }
 
 	// output the file
 
